1c0af8ce799d392bef25686bd1919caf38bb7ce4
Jonathan <jonpres@icloud.com>
2021-01-14 18:17:44 +0100

    Started on UI elements and Turret shop

---
+++ Assets/Jont/Scripts/Menu and UI/Tower Shop/scrTowerSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tower shop setting")] //Since a scriptable object is NOT placed on a GameObject, this 
+//is needed to INSTANTIATE it in our folder.
+//We are also defining its filename, though I don`t think this is necessary. This is covered on episode: 44 "Create Turrets Panel"
+public class scrTowerSettings : ScriptableObject //This is necessary for this to work as a scriptable object
+{
+    [SerializeField] private GameObject TowerPrefab;
+    [SerializeField] private int TowerShopCost;
+    [SerializeField] private Sprite TowerShopSprite;
+}
