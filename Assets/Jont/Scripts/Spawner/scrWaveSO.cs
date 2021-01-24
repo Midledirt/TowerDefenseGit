81e725dc67ebe84dee7d1d3d3b181c2f8d65a68b
Jonathan <jonpres@icloud.com>
2021-01-24 23:37:13 +0100

    A big step in the right direction

---
+++ Assets/Jont/Scripts/Spawner/scrWaveSO.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "ScriptableObject/WaveSO")]
+public class scrWaveSO : ScriptableObject
+{
+    [SerializeField] public List<GameObject> creepList;
+    [Tooltip("How many seconds from awake until this wave spawns")]
+    [SerializeField] private float waveTimer;
+    private GameObject WaveContainer;
+
+    public List<GameObject> referenceList; //Logikken (min) bak dette: 
+    //referenceList lager jeg fordi at jeg ønsker å bruke de spesefike instansene av Gameobjects som lages i InitialiazeCreeps() senere.
+    //Dette er nødvendig fordi "creepList" inneholder kun "prefabs" ikke FAKTISKE GAMEOBJETS. Så det vil ikke hjelpe å "sette dem til active(true)"
+    //senere med en "foreach loop", fordi det setter bare "prefabene" (som ikke har blitt spawnet uansett) til true.
+
+    public void InitializeCreeps()
+    {
+        referenceList = new List<GameObject>();
+
+        WaveContainer = new GameObject($"Wave Container");
+
+        foreach (GameObject creep in creepList)
+        {
+            GameObject newInstance = Instantiate(creep);
+
+            newInstance.transform.SetParent(WaveContainer.transform);
+
+            newInstance.SetActive(false);
+
+            referenceList.Add(newInstance); //Add this newly created instance of a prefab into a list that can be referenced later
+        }
+        
+    }
+
+    public void SetCreepActive()
+    {
+        foreach (GameObject creep in referenceList)
+        {
+            creep.SetActive(true);
+        }
+    }
+    
+
+    
+}
--- Assets/Jont/Scripts/Spawner/scrWaveSpawner.cs
+++ Assets/Jont/Scripts/Spawner/scrWaveSpawner.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class scrWaveSpawner : MonoBehaviour
 {
-    /*
-    private void SpawnEnemy()
+    [SerializeField] private scrWaveSO[] waveArray;
+    [SerializeField] private float timer;
+
+    private void Awake()
     {
-        //Reference the instance created by the pooler
-        GameObject newInstance = GetPooler().GetInstanceFromPool(); //This pooler needs to get some information from the new scriptable objects
-
-        Creep creep = newInstance.GetComponent<Creep>();
-        //creep.myWaypoint = waypoint;
-        //creep.ResetCreep(); //Runs a function that (at the time of writing) simply resets the creep`s end checkpoint back to 0
-
-        //Makes the creep spawn at the location of whatever object holds this script
-        creep.transform.localPosition = transform.position;
-
-        //The new instance is set to unactive by default in the pooler. This will activate it
-        newInstance.SetActive(true);
+        foreach (scrWaveSO wave in waveArray)
+        {
+            wave.InitializeCreeps(); // This should initialize the creep instances in awake, per wave
+        }
     }
-    */
+
+    private void Update()
+    {
+        if (timer >= 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                foreach (scrWaveSO wave in waveArray)
+                {
+                    wave.SetCreepActive(); //Run this function for each wave. //YES! THIS WORKS
+                }
+            }
+        }
+        
+    }
 }
